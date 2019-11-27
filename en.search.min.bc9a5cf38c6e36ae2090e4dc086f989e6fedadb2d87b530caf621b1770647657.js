45076cb6861401fc11bee94fae86f3cc0cbc9c58
Robert Kaussow <mail@geeklabor.de>
2019-11-27 13:39:33 +0000

    commit f9dc470d04c81264df5b7c508379ec566718e819 Author: Robert Kaussow <mail@geeklabor.de> Date:   Wed Nov 27 14:37:52 2019 +0100
        switch to hugo-extended

---
+++ en.search.min.bc9a5cf38c6e36ae2090e4dc086f989e6fedadb2d87b530caf621b1770647657.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.9a264324f478a486cf18f14b6ca119dad54c375681e4b50cb5a3fa87a08053f0.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
