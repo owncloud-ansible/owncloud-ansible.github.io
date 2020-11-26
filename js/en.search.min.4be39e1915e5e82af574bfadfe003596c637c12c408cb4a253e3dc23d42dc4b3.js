12cfb728fe76d011371dcd71a5ae9b4bec096f61
Robert Kaussow <mail@geeklabor.de>
2020-11-26 08:26:23 +0000

    commit fe619b5d7d10e3327ea7e6727657ea2fdf9eb10d Author: Robert Kaussow <mail@geeklabor.de> Date:   Wed Nov 11 16:43:04 2020 +0100
        update theme to v0.5.0

--- js/en.search.min.4be39e1915e5e82af574bfadfe003596c637c12c408cb4a253e3dc23d42dc4b3.js
+++ js/en.search.min.4be39e1915e5e82af574bfadfe003596c637c12c408cb4a253e3dc23d42dc4b3.js
@@ -1,4 +1,4 @@
-'use strict';(function(){const input=document.querySelector('#gdoc-search-input');const results=document.querySelector('#gdoc-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/js/flexsearch.min.js');loadScript('/js/en.search-data.min.5d41f0e4ab85bb47101498a199c7666f1d100818b07ce441a2fd48b55f0c4851.js',function(){input.required=false;search();});}
+'use strict';(function(){const input=document.querySelector('#gdoc-search-input');const results=document.querySelector('#gdoc-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/js/flexsearch.min.js');loadScript('/js/en.search-data.min.3a6b08847a289037a1968715be882bb2913ab9ce29f857dcd6b464b398f1d79a.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){console.log("empty")
 results.classList.remove("has-hits");return;}
